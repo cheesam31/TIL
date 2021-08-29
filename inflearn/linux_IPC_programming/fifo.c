93b58a8d9e884f3c55d63034fcdc57d0607b5d76
geunhyeok <cheesam31@gmail.com>
2021-08-29 18:53:44 +0900

    add inflearn/linux_IPC_programming

---
+++ inflearn/linux_IPC_programming/fifo.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define  FIFO_FILENAME	"./testfifo"
+
+static void print_usage(char *progname) 
+{
+	printf("%s (s|w)\n", progname);
+	return;
+}
+
+static int do_reader(void)
+{
+	int fd;
+	char buf[1024];
+	fd = open(FIFO_FILENAME, O_RDONLY);
+	if (fd < 0) {
+		perror("open()");
+		return -1;
+	}
+	read(fd, buf, sizeof(buf));
+	printf("writer said...%s\n", buf);
+	close(fd);
+
+	return 0;
+}
+
+static int do_writer(void)
+{
+	int fd;
+	char buf[1024];
+
+	printf("make fifo\n");
+	unlink(FIFO_FILENAME);
+	if (mkfifo(FIFO_FILENAME, 0644)) {
+		perror("mkfifo()");
+		return -1;
+	}
+
+	printf("call open(\n");
+	fd = open(FIFO_FILENAME, O_WRONLY);
+	if (fd < 0) {
+		perror("open()");
+		return -1;
+	}
+	strncpy(buf, "hello", sizeof(buf));
+	write(fd, buf, strlen(buf));
+	close(fd);
+
+	return 0;
+}
+
+/* fifo (s|w) */
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		print_usage(argv[0]);
+		return -1;
+	}
+
+	if(!strcmp(argv[1], "r")) {
+		/* reader */
+		do_reader();
+	} else if(!strcmp(argv[1], "w")) {
+		/* writer */
+		do_writer();
+	} else {
+		print_usage(argv[0]);
+		return -1;
+	}
+
+
+	return 0;
+}
